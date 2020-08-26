0fbf87e4986a45cef0bb472986241f8f80047429
douglasmatosdev <douglasmatosdev@gmail.com>
2020-08-26 15:31:21 -0300

    create control of next and previous

--- script.js
+++ script.js
@@ -4,6 +4,35 @@ function generateList() {
 
         const list = document.querySelector('#pagination .list')
         list.innerHTML = data.join("")
+    
+        return data
+}
+const data = generateList()
+let perPage = 5
+
+const state = {
+    page: 1,
+    perPage,
+    totalPages: Math.ceil(data.length / perPage)
 }
 
-generateList()
+console.log(state.totalPages)
+
+const controls = {
+    next() {
+        state.page++
+
+        const lastPage = state.page > state.totalPages
+        if(lastPage) {
+            state.page--
+        }
+    },
+    prev() {
+        state.page--
+
+        if(state.page < 1) {
+            state.page++
+        }
+    },
+    goTo() {}
+}
